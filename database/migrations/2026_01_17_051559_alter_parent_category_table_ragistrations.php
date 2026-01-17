14297e6336de047eb52e8fed606462d9c9605f11
MRgrav <gauravnetuser@gmail.com>
2026-01-17 10:57:10 +0530

    fix: typo

--- database/migrations/2026_01_17_051559_alter_parent_category_table_ragistrations.php
+++ database/migrations/2026_01_17_051559_alter_parent_category_table_ragistrations.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('ragistrations', function (Blueprint $table) {
+        Schema::table('registrations', function (Blueprint $table) {
             //
             $table->string('parents_category_b')->nullable();
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('ragistrations', function (Blueprint $table) {
+        Schema::table('registrations', function (Blueprint $table) {
             //
             $table->dropColumn('parents_category_b');
         });
