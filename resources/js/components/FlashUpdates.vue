7e9ddd7ff29d6a83dcd3e67c81aa1e0cc042756a
MRgrav <gauravnetuser@gmail.com>
2026-03-12 16:42:43 +0530

    update: flash image

--- resources/js/components/FlashUpdates.vue
+++ resources/js/components/FlashUpdates.vue
@@ -12,7 +12,7 @@
       >
         <!-- Modal content - doesn't close on backdrop click -->
         <div class="p-10 flex justify-center items-center min-h-screen">
-          <div class="relative max-w-[600px] w-[90%] mx-auto">
+          <div class="relative max-w-[700px] w-[90%] mx-auto">
             <!-- Close button -->
             <button
               class="absolute -top-12 -right-4 w-12 h-12 bg-zinc-800/70 hover:bg-zinc-900/90 rounded-full flex items-center justify-center text-white text-xl font-bold shadow-lg hover:scale-110 transition-all duration-200 z-10"
@@ -24,7 +24,7 @@
             
             <!-- Image -->
             <img 
-              src="@/../../resources/images/admission_flash.webp" 
+              src="@/../../resources/images/flash26.jpg" 
               class="w-full h-auto rounded-lg shadow-2xl"
               alt="Admission flash banner"
             />
