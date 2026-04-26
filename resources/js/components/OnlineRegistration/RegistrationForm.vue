bd6b8c52990985dcc1072c7296b7091f225e3fb8
MRgrav <gauravnetuser@gmail.com>
2026-04-26 08:50:59 +0530

    feat: update registration form submit button to disabled application full state

--- resources/js/components/OnlineRegistration/RegistrationForm.vue
+++ resources/js/components/OnlineRegistration/RegistrationForm.vue
@@ -704,10 +704,16 @@ const toggleSameAddress = () => {
     </div>
 
     <!-- SUBMIT BUTTON -->
-    <div class="flex justify-end mt-6">
+    <!-- <div class="flex justify-end mt-6">
       <button type="submit" class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         :disabled="form.processing">
         Submit
+      </button>
+    </div> -->
+    <div class="flex justify-end mt-6">
+      <button type="reset" class="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+        :disabled="true">
+        Application Full
       </button>
     </div>
   </form>
